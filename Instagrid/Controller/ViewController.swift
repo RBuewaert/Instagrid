6d6a53d63e32e3edadc35ecd0fc4efc66ff698ab
Romain Buewaert <romain.buewaert@gmail.com>
2021-05-06 09:50:22 +0200

    Modification of function chooseImage and imagePickerController to update size of image (objective: not to distort the image when it is inserted) and add comment

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -18,10 +18,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-            
+    
         currentLayout = .one
-        
         
         // Swipe for Portrait orientation
         let leftSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare(_:)))
@@ -32,42 +30,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let upSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare(_:)))
         upSwipeGestureRecognizer.direction = .up
         mainView.addGestureRecognizer(upSwipeGestureRecognizer)
-        
     }
     
     var currentLayout: Layout = .one {
         didSet {
-            updateView()
+            updateMainView()
         }
     }
-    
-    @IBOutlet weak var swipeToShareLabel: UILabel! {
-        didSet {
-            if UIApplication.shared.statusBarOrientation.isPortrait {
-                self.swipeToShareLabel.text = "Swipe up to share"
-            } else {
-                self.swipeToShareLabel.text = "Swipe left to share"
-            }
-        }
-    }
-    
-    func editLabel(Label : UILabel) {
-        if UIApplication.shared.statusBarOrientation.isPortrait {
-            Label.text = "swipe up to share"
-        } else {
-            Label.text = "swipe left to share"
-        }
-    }
-    
-    
-    @IBOutlet weak var arrowToShare: UIImageView!
-    
+    var currentButton: UIButton?
     
     @IBOutlet weak var mainView: UIView!
     
-    @IBOutlet weak var photo1: UIButton!
     @IBOutlet weak var photo2: UIButton!
-    @IBOutlet weak var photo3: UIButton!
     @IBOutlet weak var photo4: UIButton!
     
     @IBOutlet weak var layout1: UIButton!
@@ -86,43 +60,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         currentLayout = .three
     }
     
-    var currentButton: UIButton?
+    // Function valid for the 4 photo buttons
     @IBAction func addPhoto(_ sender: UIButton ) {
         sender.isSelected = true
         currentButton = sender
         chooseImage()
     }
     
-//    @IBAction func addPhoto1(_ sender: UIButton) {
-//        sender.isSelected = true
-//        chooseImage()
-//    }
-//
-//    @IBAction func addPhoto2(_ sender: UIButton) {
-//        sender.isSelected = true
-//        chooseImage()
-//    }
-//
-//    @IBAction func addPhoto3(_ sender: UIButton) {
-//        sender.isSelected = true
-//        chooseImage()
-//    }
-//
-//    @IBAction func addPhoto4(_ sender: UIButton) {
-//        sender.isSelected = true
-//        chooseImage()
-//    }
     
-    var choosedlmage: UIImage!
-    
-    
-    
-    var orientation: UIDeviceOrientation!
-    
-   
-    
-    
-    func updateView() {
+    func updateMainView() {
         let layoutButtons = [layout1, layout2, layout3]
         for button in layoutButtons {
             button?.isSelected = false
@@ -143,42 +89,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-//    func selectLayout(withPortraitButton: UIButton, withLandscapeButton: UIButton, topViewIshidden: Bool, bottomViewIsHidden: Bool) {
-//        let buttons = [layout1P, layout1L, layout2P, layout2L, layout3P, layout3L]
-//        for button in buttons {
-//            button!.isSelected = false
-//        }
-//        withPortraitButton.isSelected = true
-//        withLandscapeButton.isSelected = true
-//        if topViewIshidden == true {
-//            photo2.isHidden = true
-//        } else {
-//            photo2.isHidden = false
-//        }
-//        if bottomViewIsHidden == true {
-//            photo4.isHidden = true
-//        } else {
-//            photo4.isHidden = false
-//        }
-//    }
-    
-    private func changeImage() {
-        currentButton?.setImage(choosedlmage, for: .normal)
-//        if photo1.isSelected == true {
-//            photo1.setImage(choosedlmage, for: .normal)
-//            photo1.isSelected = false
-//        } else if photo2.isSelected == true {
-//            photo2.setImage(choosedlmage, for: .normal)
-//            photo2.isSelected = false
-//        } else if photo3.isSelected == true {
-//            photo3.setImage(choosedlmage, for: .normal)
-//            photo3.isSelected = false
-//        } else { // photo4.isSelected == true
-//            photo4.setImage(choosedlmage, for: .normal)
-//            photo4.isSelected = false
-//        }
-    }
-    
+    // Show action sheet for choose a photo
     func chooseImage() {
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
@@ -208,19 +119,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.present(actionSheet, animated: true, completion: nil)
     }
 
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        picker.dismiss(animated: true, completion: nil)
+//    }
 
+    // Replace image for currentButton (continuity of function chooseImage)
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         currentButton?.setImage((info[UIImagePickerController.InfoKey.originalImage] as! UIImage), for: .normal)
-//        choosedlmage = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
-//        changeImage()
+//        equivalent to: let choosedlmage = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
+//        currentButton?.setimage(choosedImage, for .normal)
+        currentButton?.imageView?.contentMode = . scaleAspectFill
+        currentButton?.contentVerticalAlignment = .fill
+        currentButton?.contentHorizontalAlignment = .fill
         picker.dismiss(animated: true, completion: nil)
     }
     
+    // Function to define the use of the UISwipeGestureRecognizer and the result
     @objc func swipeToShare(_ sender: UISwipeGestureRecognizer) {
-        
         if UIApplication.shared.statusBarOrientation.isPortrait && sender.direction == .left {
             return
         }
@@ -239,8 +154,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                         self.launchUIActivityController()
                     }
                 })
-
-            } else {
+            } else { // isLandscape
                 let translation = CGAffineTransform(translationX: -screenWidth, y: 0)
                 UIView.animate(withDuration: 0.5, animations: {
                     self.mainView.transform = translation
@@ -249,13 +163,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                         self.launchUIActivityController()
                     }
                 })
-
             }
         }
     }
         
+    // Function to launch the UIActivityController to share
     private func launchUIActivityController() {
-        
         if let imageToShare = imageWithView(view: mainView) {
             let activityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
             activityViewController.popoverPresentationController?.sourceView = self.view
@@ -266,33 +179,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                     self.mainView.transform = .identity
                 }, completion: nil)
             }
-//            activityViewController.CompletionHandler {
-//                returnToMainScreen() }
-//
-//            activityViewController.completionWithItemsHandler = { (success) in
-//                if success {
-//                    self.returnToMainScreen()
-//                }
-//            }
-            
         }
-//        UIActivityViewController.CompletionHandler.self: returnToMainScreen()
-        
-//        UIView.animate(withDuration: 1, animations: {
-//            self.mainView.transform = .identity
-//        }, completion: nil)
-       
     }
-    
-    private func returnToMainScreen() {
-        UIView.animate(withDuration: 1, animations: {
-            self.mainView.transform = .identity
-        }, completion: nil)
 
-    }
-    
-    
-    
+    // Function to convert UIView to UIImage (utility to share an image for the user)
     private func imageWithView(view: UIView) -> UIImage? {
             UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0)
             view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
@@ -300,66 +190,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             UIGraphicsEndImageContext()
             return img
         }
-        
-    
-    
-    
-    
-    
-    
-    
-        
-        
-//        switch orientation {
-//        case .portrait:
-//            gesture.direction = .up
-//            mainView.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height)
-//
-//
-//        case .landscapeLeft, .landscapeRight:
-//            gesture.direction = .left
-//            mainView.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
-//
-//
-//        default:
-//            break
-//        }
-        
-//        private func transformQuestionViewWith(gesture: UIPanGestureRecognizer){
-//            let translation = gesture.translation(in: questionView)
-//            let translationTransform = CGAffineTransform(translationX: translation.x, y: translation.y)
-//
-//            let translationPercent = translation.x / (UIScreen.main.bounds.width / 2)
-//            let rotationAngle = (CGFloat.pi / 3) * translationPercent
-//            let rotationTransform = CGAffineTransform(rotationAngle: rotationAngle)
-//
-//            let transform = translationTransform.concatenating(rotationTransform)
-//            questionView.transform = transform
-//
-//            if translation.x > 0 {
-//                questionView.style = .correct
-//            } else {
-//                questionView.style = .incorrect
-//            }
-//        }
-//
-        
-
-    
-    
-    
-    
-//    @objc func upSwipeToShare(_ sender: UISwipeGestureRecognizer) {
-//        switch sender.state {
-//        case .began :
-//            upTransformMainViewWith(gesture: sender)
-//        case .ended :
-//            launchUIActivityController()
-//        default:
-//            break
-//        }
-//    }
-
-   
 }
 
